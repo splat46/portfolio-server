056ceeb84e9b793bf5d6f964e352c74bb6e8ab48
splat46 <stijnhuiskes@gmail.com>
2020-08-31 10:18:18 +0200

    setup login and signup endpoints

--- routers/auth.js
+++ routers/auth.js
@@ -7,27 +7,62 @@ const User = require("../models").user;
 const router = new Router();
 
 router.post("/login", async (req, res, next) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(400).send("Please supply a valid email and password");
-  } else {
-    const user = await User.findOne({
-      where: { email },
-    });
-    if (!user) {
-      res.status(400).send({
-        message: "User with that email does not exist",
-      });
-    } else if (bcrypt.compareSync(password, user.password)) {
-      const jwt = toJWT({ userId: user.id });
-      res.send({
-        jwt,
-      });
-    } else {
-      res.status(400).send({
-        message: "Password was incorrect",
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: "Please provide both email and password" });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(400).send({
+        message: "User with that email not found or password incorrect",
       });
     }
+
+    delete user.dataValues["password"]; // don't send back the password hash
+    const token = toJWT({ userId: user.id });
+    return res.status(200).send({ token, ...user.dataValues });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
+router.post("/signup", async (req, res) => {
+  const { email, password, firstname, lastname, phone } = req.body;
+  if (!email || !password || !firstname || !lastname || !phone) {
+    return res
+      .status(400)
+      .send("Please provide an email, password and a fullname and phone");
+  }
+
+  try {
+    const newUser = await User.create({
+      email,
+      password: bcrypt.hashSync(password, SALT_ROUNDS),
+      firstname,
+      lastname,
+      phone,
+    });
+
+    delete newUser.dataValues["password"]; // don't send back the password hash
+
+    const token = toJWT({ userId: newUser.id });
+
+    res.status(201).json({ token, ...newUser.dataValues });
+  } catch (error) {
+    if (error.email === "SequelizeUniqueConstraintError") {
+      return res
+        .status(400)
+        .send({ message: "There is an existing account with this email" });
+    }
+
+    return res.status(400).send({ message: "Something went wrong, sorry" });
   }
 });
 
